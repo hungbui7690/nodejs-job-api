9b2acc294a2ca82afc9109331261b57936e0bbda
hungbui7690 <hungbui7690@gmail.com>
2022-12-26 03:30:23 -0700

    error handler: validation, cast, duplication + security packages

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 // IMPORT
 // =============================================
 
+// security
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
 // packges
 require('dotenv').config()
 require('express-async-errors')
@@ -23,6 +29,19 @@ const authenticationMiddleware = require('./middleware/authentication')
 // =============================================
 // MIDDLEWARES & ROUTES
 // =============================================
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// security
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(limiter)
 
 app.use(express.json())
 
